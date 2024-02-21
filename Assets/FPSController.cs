fe7719bccde4a0d90ce110e4de5d7475ec652841
Radeah <113103602+Radeah@users.noreply.github.com>
2024-02-21 22:27:27 +0000

    checking something

---
+++ Assets/FPSController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float mouseSensitivity = 100f;
+
+    private CharacterController controller;
+    private Camera playerCamera;
+    private float verticalRotation = 0f;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        playerCamera = GetComponentInChildren<Camera>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        // Rotation
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        verticalRotation -= mouseY;
+        verticalRotation = Mathf.Clamp(verticalRotation, -90f, 90f);
+
+        playerCamera.transform.localRotation = Quaternion.Euler(verticalRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+
+        // Movement
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * moveSpeed * Time.deltaTime);
+    }
+}
